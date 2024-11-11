36130c1dd9a0d33619588fc078dabc622b267a4d
ZyadHany35 <86461552+ZyadHany35@users.noreply.github.com>
2024-11-12 01:46:09 +0200

    Adding Class car

--- src/Classes/Car.java
+++ src/Classes/Car.java
@@ -15,8 +15,54 @@ class Car extends Thread {
         this.parkingLot = parkingLot;
     }
 
+    public int getCarNum() {
+        return carNum;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public int getParkTime() {
+        return parkTime;
+    }
+
+    public int getGateNum() {
+        return gateNum;
+    }
+
+    public ParkingLot getParkingLot() {
+        return parkingLot;
+    }
+
+    public void setCarNum(int carNum) {
+        this.carNum = carNum;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public void setParkTime(int parkTime) {
+        this.parkTime = parkTime;
+    }
+
+    public void setGateNum(int gateNum) {
+        this.gateNum = gateNum;
+    }
+
+    public void setParkingLot(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
     @Override
     public void run() {
+        try {
+            Thread.sleep(arrivalTime * 1000L);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+        LogAndReport.logArrive(carNum, gateNum, arrivalTime);
         parkingLot.tryToPark(carNum, gateNum, parkTime);
     }
 }
