d1528f91f1830e757bce88e11bce4170395f6ba9
ZyadHany35 <86461552+ZyadHany35@users.noreply.github.com>
2024-11-11 23:08:54 +0200

    Update LogAndReport.java
    adding parkAfterwait
    and modify some methods

--- src/Classes/LogAndReport.java
+++ src/Classes/LogAndReport.java
@@ -1,32 +1,34 @@
 package org.ps;
 // How to use make and instance of LogAndReport in your class and call logger.method you want to logging status
 public class LogAndReport {
-    private int totalCarsServed = 0;
-    private final int[] gateCarsServed = new int[3];
+    private static int totalCarsServed = 0;
+    private static final int[] gateCarsServed = new int[3];
 
-    public synchronized void logArrive(int carNum, int gateNum,int arriveTime) {
+    public static synchronized void logArrive(int carNum, int gateNum,int arriveTime) {
         System.out.println("Car " + carNum + " from Gate " + gateNum + " arrived at time " + arriveTime );
     }
 
-    public synchronized void logPark(int carNum, int gateNum, int occupiedSpots) {
+    public static synchronized void logPark(int carNum, int gateNum, int occupiedSpots) {
         totalCarsServed++;
         gateCarsServed[gateNum - 1]++;
         System.out.println("Car " + carNum + " from Gate " + gateNum + " parked. (Parking Status: " + occupiedSpots + " spots occupied)");
     }
 
-    public synchronized void logWait(int carNum, int gateNum, int waitTime) {
-        if (waitTime > 0) {
-            System.out.println("Car " + carNum + " from Gate " + gateNum + " waited for " + waitTime + " parked. (Parking Status: 4 spots occupied)");
-        } else {
-            System.out.println("Car " + carNum + " from Gate " + gateNum + " waiting for a spot.");
-        }
+    public static synchronized void logWait(int carNum, int gateNum) {
+        System.out.println("Car " + carNum + " from Gate " + gateNum + " waiting for a spot.");
     }
 
-    public synchronized void logLeaving(int carNum, int gateNum, int parkTime, int occupiedSpots) {
+    public static synchronized void logParkAfterWaiting(int carNum, int gateNum, int occupiedSpots,long waiting) {
+        totalCarsServed++;
+        gateCarsServed[gateNum - 1]++;
+        System.out.println("Car " + carNum + " from Gate " + gateNum +" after waiting for " + waiting + " units of time parked. (Parking Status: " + occupiedSpots + " spots occupied)");
+    }
+
+    public static synchronized void logLeaving(int carNum, int gateNum, int parkTime, int occupiedSpots) {
         System.out.println("Car " + carNum + " from Gate " + gateNum + " left after " + parkTime + " units of time. (Parking Status: " + occupiedSpots + " spots occupied)");
     }
 
-    public void printSummary() {
+    public static void printSummary() {
         System.out.println("\nTotal Cars Served: " + totalCarsServed);
         System.out.println("Current Cars in Parking: 0");
         System.out.println("Details:");
