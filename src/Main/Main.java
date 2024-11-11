cbe3038a1693d1ba8ed190f897fb7643515116aa
ZyadHany35 <86461552+ZyadHany35@users.noreply.github.com>
2024-11-12 01:45:41 +0200

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,6 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        ParkingLot parkingLot = new ParkingLot(4);
+        LogAndReport logger = new LogAndReport();
+        Gate gates[] = new Gate[3];
+
+        ParkingSystem parkingSystem = new ParkingSystem(parkingLot, logger, gates, "carOutputFile.txt");
+
+        parkingSystem.runParkingSystem();
+    }
 
 }
