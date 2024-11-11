2a8c52235e3f75da2b1c2a119932bb6e663d21c2
ZyadHany35 <86461552+ZyadHany35@users.noreply.github.com>
2024-11-12 01:46:29 +0200

    Adding Class Gate

--- src/Classes/Gate.java
+++ src/Classes/Gate.java
@@ -1,5 +1,40 @@
 package org.ps;
 
-class Gate {
+import java.util.List;
 
+public class Gate implements Runnable {
+    private int gateNum;
+    private List<Car> cars;
+    private ParkingLot parkingLot;
+
+    public Gate(int gateNum, List<Car> cars, ParkingLot parkingLot) {
+        this.gateNum = gateNum;
+        this.cars = cars;
+        this.parkingLot = parkingLot;
+    }
+
+    @Override
+    public void run() {
+        for (Car car : cars){
+            car.start();
+        }
+        for (Car car : cars)
+            try {
+                car.join();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+
+    }
 }
+
+//    public void processCarArrival(int carId, int arrivalTime, int parkTime) {
+//        Car car = new Car(carId, gateNum, parkTime, parkingLot, logger);
+//        try {
+//            Thread.sleep(arrivalTime * 1000);
+//            car.start();
+//        } catch (InterruptedException e) {
+//            Thread.currentThread().interrupt();
+//        }
+//    }
+
