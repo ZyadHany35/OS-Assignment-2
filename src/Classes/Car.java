eae842d501e210592925560a8c5b22b7e78df62e
ZyadHany35 <86461552+ZyadHany35@users.noreply.github.com>
2024-11-11 23:11:33 +0200

    Update Car.java

--- src/Classes/Car.java
+++ src/Classes/Car.java
@@ -1,5 +1,22 @@
 package org.ps;
 
 class Car extends Thread {
+    private int carNum;
+    private int arrivalTime;
+    private int parkTime;
+    private int gateNum;
+    private ParkingLot parkingLot;
 
+    public Car(int carNum, int gateNum, int parkTime,int arrivalTime, ParkingLot parkingLot) {
+        this.carNum = carNum;
+        this.arrivalTime = arrivalTime;
+        this.parkTime = parkTime;
+        this.gateNum = gateNum;
+        this.parkingLot = parkingLot;
+    }
+
+    @Override
+    public void run() {
+        parkingLot.tryToPark(carNum, gateNum, parkTime);
+    }
 }
